012a892e0734f599fa136ac9f6da7cdcfbdce747
Wesley Chen <weschen1996@gmail.com>
2020-01-17 20:35:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,9 +3,9 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  return array.uniq().sort().last
+  return array.sort().last
 end
 
 def find_min_value(array)
-  return array.uniq().sort().first
+  return array.sort().first
 end
